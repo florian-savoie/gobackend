7763908dcb3c9732102005cf4631daba326fad4e
florian-savoie <florianpsp303@gmail.com>
2023-09-04 22:03:41 +0200

    maj

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const Thing = require('./models/thing');
 const dotenv = require('dotenv');
 dotenv.config();
+
+
 mongoose.connect(process.env.MONGO_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -30,5 +32,29 @@ app.use((req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
-  
+  app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+  app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({_id: req.params.id}, { ...req.body,_id: req.params.id})
+      .then(things => res.status(200).json(things))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id})
+      .then(things => res.status(200).json({message : 'objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+
 module.exports = app;
