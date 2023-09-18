e960ee61af7a558244a8d656c8a2d778e26cba82
florian-savoie <florianpsp303@gmail.com>
2023-09-18 23:04:18 +0200

    maj

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req,res,next) => {
  bcrypt.hash(req.body.password,10)
@@ -15,6 +16,39 @@ exports.signup = (req,res,next) => {
  })
 .catch(err => res.status(500).json({error}));
 };
-exports.login = (req,res,next) => {
 
+
+exports.login = (req,res,next) => {
+User.findOne({email:req.body.email})   
+.then(user => {
+  if (user ===null){
+    res.status(401).json({
+      message : 'paire identifiant/mot de passe incorrecte'
+  });}
+  else{
+    bcrypt.compare(req.body.password,user.password)
+    .then(valid => {
+      if(!valid){
+        res.status(401).json({
+          message : 'paire identifiant/mot de passe incorrecte'
+        });
+      }else{
+        res.status(200).json({
+           userId : user._id,
+           token : jwt.sign(
+            { userId : user._id },
+            'RANDOM_TOKEN_SECRET',
+            {expiresIn: '24h'}
+           )
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error});
+    })
+  }
+})
+.catch(err => {
+  res.status(500).json({error});
+})
 };
