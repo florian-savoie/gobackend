c2e88d9896643fe48182e79bd89c9a6da71bbdcf
florian-savoie <florianpsp303@gmail.com>
2023-09-19 22:26:53 +0200

    maj

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const auth = require('../middleware/auth')
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 
-  router.post('/',stuffCtrl.createThing );
-  router.get('/:id', stuffCtrl.getOneThing);
-  router.get('/', stuffCtrl.getAllThings);
-  router.put('/:id',stuffCtrl.modifyThing);
-  router.delete('/:id', stuffCtrl.deleteThing);
+  router.post('/',auth , stuffCtrl.createThing );
+  router.get('/:id',auth ,  stuffCtrl.getOneThing);
+  router.get('/',auth ,  stuffCtrl.getAllThings);
+  router.put('/:id',auth , stuffCtrl.modifyThing);
+  router.delete('/:id',auth ,  stuffCtrl.deleteThing);
 
   
   module.exports = router ;
